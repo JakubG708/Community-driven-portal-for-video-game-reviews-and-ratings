18717ad080035c0780355c329a54144402ef6de7
Jakub Gnela <agnel113322@gmail.com>
2025-10-12 15:19:38 +0200

    games controller

---
+++ GamesPlatform/GamesPlatform/Controllers/GamesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamesPlatform.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
